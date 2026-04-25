2f8e380a8869fc6aa95fc9d5b95f13887b25a9fa
cankocak <can_kocak1@epam.com>
2026-04-25 09:28:02 +0300

    clone is inefficient normally but works for now

--- src/refactoring.rs
+++ src/refactoring.rs
@@ -63,3 +63,54 @@
 ///         (query, file_path)
 ///     }
 ///     Listing 12-5: Extracting a parse_config function from main
+/// We're still collecting the command line arguments into a vector, but instead of assigning the
+/// argument value at index 1 to the variable query and the argument value at index 2 to the
+/// variable file_path within the main function, we pass the whole vector to the parse_config
+/// function. The parse_config function then holds the logic that determines which argument goes in
+/// which variable and passes the value back to main. We still create the query and file_path
+/// variables in main, but main no longer has the responsibility of determining how the command line
+/// arguments and variables correspond.
+///
+/// Grouping Configuration Values
+///
+/// We can take another small step to improve the parse_config function further. At the moment,
+/// we're returning a tuple, but when we immediately break that tuple into individual parts again.
+/// This is a sign that perhaps we don't have the right abstraction yet.
+///
+/// Another indicator that shows there's room for improvement is the config part of parse_config,
+/// which implies that the two values we return are related and are both part of one configuration
+/// value. We're not currently conveying this meaning in the structure of the data other than by
+/// grouping the two values into a tuple; we'll instead put the two values into one struct and give
+/// each of the struct fields a meaningful name. Doing so will make it easier for future maintainers
+/// of this code to understand how the different values relate to each other and what their purpose
+/// is.
+///
+/// We've added a struct named Config defined to have fields named query and file_path. The
+/// signature of parse_config now indicates that it returns a Config value. In the body of
+/// parse_config, where we used to return string slices that reference String values in args, we now
+/// define Config to contain owned String values. The args variable in main is the owner of the
+/// argument values and is only letting the parse_config function borrow them, which means we'd
+/// violate Rust's borrowing rules if Config tried to take ownership of the values in args.
+///
+/// There are a number of ways we could manage String data; the easiest, though somewhat
+/// inefficient, route is to call the clone method on the values. This will make a full copy of the
+/// data for the Config instance to own, which takes more time and memory than sotring a reference
+/// to the string data. However, cloning the data also makes our code very straightforward because
+/// we don't have to manage the lifetimes of the references; in this circumstance, giving up a
+/// little performance to gain simplicity is a worthwhile trade-off.
+///
+/// The Trade-Offs of Using clone
+///
+/// There's a tendency among many Rustaceans to avoid using clone to fix ownership problems because
+/// of its runtime cost. There are more efficient ways to do this but for now it's okay to copy a
+/// few strings to continue making progress because you'll make these copies only once and your file
+/// path and query string are very small. It's better to have a working program that's a bit
+/// inefficient than to try to hyperoptimize code on your first pass.
+///
+/// We've updated main so it places the instace of Config returned by parse_config into a variable
+/// named config, and we updated the code that previously used the separate query and file_path
+/// variables so it now uses the fields on the Config struct instead.
+///
+/// Now our code more clearly conveys that query and file_path are related and that their purpose is
+/// to configure how the program will work. Any code that uses these values knows to find them in
+/// the config instance in the fields named for their purpose.
